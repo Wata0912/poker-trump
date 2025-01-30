62af00c466bd0ff82208116c9685e298e4243799
Wata0912 <241211@st.yoshida-g.ac.jp>
2025-01-30 10:35:00 +0900

    ファイル作成

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokergame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+            while (true)
+            {
+                
+            }
+        }
+    }
+}
