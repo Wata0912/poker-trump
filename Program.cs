ce361c16254b1e18edb7663ee1b643d905af5811
Wata0912 <241211@st.yoshida-g.ac.jp>
2025-01-30 11:05:26 +0900

    トランプの数字の入力メソットを設定

--- Program.cs
+++ Program.cs
@@ -11,12 +11,36 @@ namespace pokergame
     {
         static void Main(string[] args)
         {
-           
+           int [] cards = new int [4];
 
+            string num;
+            bool result ;
 
             while (true)
             {
-                
+                for (int i = 0; i < cards.Length; i++)
+                {
+                    while(true)
+                    {
+                        Console.WriteLine($"{i + 1}番目のカードの数字を入力してください＞");
+                        num = Console.ReadLine();
+                        result = int.TryParse(num, out cards[i]);
+                        if (result == true)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("再入力してください");
+                        }
+                    }
+                    
+                }
+
+
+
+
+
             }
         }
     }
